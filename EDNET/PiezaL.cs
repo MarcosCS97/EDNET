504aec1a75943eaca4426a9214e5c0433de8c2a5
Marcos Caldas <qazedcwsxmarcos@gmail.com>
2019-04-15 21:06:45 +0200

    Creada la clase PiezaL

--- EDNET/PiezaL.cs
+++ EDNET/PiezaL.cs
@@ -7,43 +7,43 @@ using System.Threading.Tasks;
 
 namespace EDNET
 {
-    class PiezaJ : Pieza
+    class PiezaL : Pieza
     {
-        public PiezaJ(int separac, int tamCuad, GraphicsDeviceManager graphics) : base(separac, tamCuad, graphics)
+        public PiezaL(int separac, int tamCuad, GraphicsDeviceManager graphics) : base(separac, tamCuad, graphics)
         {
-            color = Color.Blue;
+            color = Color.Orange;
         }
         public override void rotaPieza()
         {
             switch (rotac)
             {
                 case 1:
-                    for(int i=0; i<3; i++)
+                    for (int i = 0; i < 3; i++)
                     {
-                        cuadrados[i].Location = new Point(calcPos[i]((int)posic.X), calcPos[1]((int)posic.Y)); 
+                        cuadrados[i].Location = new Point(calcPos[i]((int)posic.X), calcPos[1]((int)posic.Y));
                     }
-                    cuadrados[3].Location = new Point(calcPos[0]((int)posic.X), calcPos[0]((int)posic.Y));
+                    cuadrados[3].Location = new Point(calcPos[2]((int)posic.X), calcPos[0]((int)posic.Y));
                     break;
                 case 2:
-                    for(int i = 0; i < 3; i++)
+                    for (int i = 0; i < 3; i++)
                     {
-                        cuadrados[i].Location = new Point(calcPos[0]((int)posic.X), calcPos[i]((int)posic.Y)); 
+                        cuadrados[i].Location = new Point(calcPos[0]((int)posic.X), calcPos[i]((int)posic.Y));
                     }
-                    cuadrados[3].Location = new Point(calcPos[1]((int)posic.X), calcPos[0]((int)posic.Y)); 
+                    cuadrados[3].Location = new Point(calcPos[1]((int)posic.X), calcPos[2]((int)posic.Y));
                     break;
                 case 3:
                     for (int i = 0; i < 3; i++)
                     {
                         cuadrados[i].Location = new Point(calcPos[i]((int)posic.X), calcPos[0]((int)posic.Y));
                     }
-                    cuadrados[3].Location = new Point(calcPos[2]((int)posic.X), calcPos[1]((int)posic.Y));
+                    cuadrados[3].Location = new Point(calcPos[0]((int)posic.X), calcPos[1]((int)posic.Y));
                     break;
                 case 4:
                     for (int i = 0; i < 3; i++)
                     {
                         cuadrados[i].Location = new Point(calcPos[1]((int)posic.X), calcPos[i]((int)posic.Y));
                     }
-                    cuadrados[3].Location = new Point(calcPos[0]((int)posic.X), calcPos[2]((int)posic.Y));
+                    cuadrados[3].Location = new Point(calcPos[0]((int)posic.X), calcPos[0]((int)posic.Y));
                     break;
             }
             rotac++;
